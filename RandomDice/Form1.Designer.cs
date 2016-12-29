9a77fa4504bf82f50dd52441bfe735a746b619ec
ufuk <ufuk@UFUKDUNDAR>
2016-12-29 12:52:46 +0300

    Updated.

--- RandomDice/Form1.Designer.cs
+++ RandomDice/Form1.Designer.cs
@@ -28,12 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Dice1 = new System.Windows.Forms.PictureBox();
+            this.Dice2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Dice1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dice2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Dice1
+            // 
+            this.Dice1.Location = new System.Drawing.Point(47, 29);
+            this.Dice1.Name = "Dice1";
+            this.Dice1.Size = new System.Drawing.Size(140, 140);
+            this.Dice1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Dice1.TabIndex = 0;
+            this.Dice1.TabStop = false;
+            this.Dice1.Click += new System.EventHandler(this.Dice1_Click);
+            // 
+            // Dice2
+            // 
+            this.Dice2.Location = new System.Drawing.Point(314, 29);
+            this.Dice2.Name = "Dice2";
+            this.Dice2.Size = new System.Drawing.Size(140, 140);
+            this.Dice2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Dice2.TabIndex = 1;
+            this.Dice2.TabStop = false;
+            this.Dice2.Click += new System.EventHandler(this.Dice2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 364);
+            this.Controls.Add(this.Dice2);
+            this.Controls.Add(this.Dice1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Dice1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dice2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Dice1;
+        private System.Windows.Forms.PictureBox Dice2;
     }
 }
 
--- RandomDice/Form1.cs
+++ RandomDice/Form1.cs
@@ -16,5 +16,30 @@ namespace RandomDice
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Dice1.ImageLocation= System.IO.Path.GetFullPath("Pictures/Dice-0.png");
+            Dice2.ImageLocation= System.IO.Path.GetFullPath("Pictures/Dice-0.png");
+        }
+        public void RollDice()
+        {
+            Random rnd = new Random();
+            int diceValue1 = rnd.Next(1, 7);
+            int diceValue2 = rnd.Next(1, 7);
+
+            Dice1.ImageLocation = System.IO.Path.GetFullPath("Pictures/Dice-"+diceValue1.ToString()+ ".png");
+            Dice2.ImageLocation = System.IO.Path.GetFullPath("Pictures/Dice-"+diceValue2.ToString()+".png");
+        }
+
+        private void Dice1_Click(object sender, EventArgs e)
+        {
+            RollDice();
+        }
+
+        private void Dice2_Click(object sender, EventArgs e)
+        {
+            RollDice();
+        }
     }
 }
